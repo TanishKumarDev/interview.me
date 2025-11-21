fed5c605bb4fb7c0369700a92348aa2f27407c9a
Tanish <tanish.kumar.contact@gmail.com>
2025-11-22 05:24:03 +0530

    feat: initialize frontend with React and Vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h2>Interview.me</h2>
+     <p>Interview.me AI based interview preparation platform</p>
+    </>
+  )
+}
+
+export default App
